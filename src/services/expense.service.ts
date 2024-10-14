090d5b4ad5da831fe296a61bb8b7361c2a742c58
boluwatife010 <temitopeboluojo@gmail.com>
2024-10-14 15:20:51 +0100

    feat: working on expense tracker

--- src/services/expense.service.ts
+++ src/services/expense.service.ts
@@ -1,5 +1,6 @@
 import { expenseTrackerModel } from "../models/expensetracker.model";
 import { expenseRequestBody, updateRequestBody } from "../interfaces/expenses";
+import { userModel } from "../models/user.model";
 export const createNewExpense = async (body: expenseRequestBody): Promise<any> => {
     const {title, amount, categories, date} = body
     if(!title && !amount && !categories && !date) {
@@ -54,3 +55,28 @@ export const deleteExpense = async (id: string): Promise<any> => {
     }
     return remove
 }
+// Create filter endpoint
+export const filterExpense = async (user: any, period?: string, startDate?: string, endDate?: string): Promise<any> => {
+    const today = new Date();
+    const filter: any = { userId: user._id }; 
+    if (period) {
+        switch (period) {
+            case 'week':
+                filter.date = { $gte: new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000) };
+                break;
+            case 'month':
+                filter.date = { $gte: new Date(today.setMonth(today.getMonth() - 1)) };
+                break;
+            case '3months':
+                filter.date = { $gte: new Date(today.setMonth(today.getMonth() - 3)) };
+                break;
+        }
+    } else if (startDate && endDate) {  
+        filter.date = {
+            $gte: new Date(new Date(startDate).setHours(0, 0, 0, 0)),
+            $lte: new Date(new Date(endDate).setHours(23, 59, 59, 999)) 
+        };
+    }
+    const expenses = await expenseTrackerModel.find(filter).exec();
+    return expenses;
+};
